49086606ce8f6bda519ca9e23effc3ecfee3f4ee
nanayyayaa <jiejueejuju@gmail.com>
2026-04-27 17:52:47 +0700

    commit 7

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -5,6 +5,16 @@ using namespace std;
 class siswa;
 class orang
 {
-    private:
-        string nama;
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
 }
