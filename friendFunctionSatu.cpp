3a551b63d5119d50d2e143628ca9297f10d1912a
nanayyayaa <jiejueejuju@gmail.com>
2026-04-27 18:06:19 +0700

    commit 11

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -4,4 +4,12 @@ using namespace std;
 class mahasiswa
 {
 private:
+    string nama;
+public:
+    friend void setNama(mahasiswa &a, string b)
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
 }
